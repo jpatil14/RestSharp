3f2141069987d18882c5c4a1a992972860771c2a
patilja <jatin.patil@schroders.com>
2019-06-12 16:19:33 +0100

    office commit 1206

--- RestSharpDemo/UnitTest1.cs
+++ RestSharpDemo/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
@@ -14,14 +15,27 @@ namespace RestSharpDemo
     {
         [TestMethod]
         public void PostWithTypeClassBody()
-        { 
-            var client = new RestClient("https://oms-dev-dv1.plt-l-dev-uks-ase.azure.schroders.com/api/DefaultFunction1");
-            var request = new RestRequest("get", Method.GET);
+        {
+            //var client = new RestClient("https://reqres.in/api/users/2");
+            //var request = new RestRequest("put", Method.PUT);
+            //var file = @"TestData\TestData.json";
+
             //request.RequestFormat = DataFormat.Json;
-            //request.AddBody(new Posts() { name = "Jatin", job = "test analyst",id="90" });
-            var response = client.Execute(request);
-            var statuscode=response.StatusCode.ToString();
-            
+            //var jsonData= Newtonsoft.Json.JsonConvert.DeserializeObject<UserBody>(File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file)).ToString());
+            //request.AddJsonBody(jsonData);
+            //var restResponse = client.Execute(request);
+            //var statuscode = restResponse.GetResponseStatusCode();
+
+            var client = new RestClient("https://oms-dev-dv1.plt-l-dev-uks-ase.azure.schroders.com/api/InspectAladdinJson");
+            var request = new RestRequest(Method.GET);
+            //var file = @"TestData\TestData.json";
+
+            //request.RequestFormat = DataFormat.Json;
+            //var jsonData = Newtonsoft.Json.JsonConvert.DeserializeObject<UserBody>(File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file)).ToString());
+            //request.AddJsonBody(jsonData);
+            var restResponse = client.Execute(request);
+            var statuscode = restResponse.GetResponseStatusCode();
+
             //var desrialize = new JsonDeserializer();
             //var output = desrialize.Deserialize<Dictionary<string, string>>(response);
             //var result = output["name"];
@@ -44,6 +58,10 @@ namespace RestSharpDemo
 
         }
 
-        
+        private class UserBody
+        {
+            public string name { get; set; }
+            public string job { get; set; }
+        }
     }
 }
